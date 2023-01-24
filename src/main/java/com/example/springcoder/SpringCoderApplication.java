9c37b50e2996ddccb9fd0b5ca787fc3e48ccfa4e
chq-payalk <payal.kamble@expeditors.com>
2023-01-24 11:51:16 -0800

    Initial commit

---
+++ src/main/java/com/example/springcoder/SpringCoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcoder/SpringCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
