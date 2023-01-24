ef0cd2f54c3edf43889ac5bf0b27f312c9dfcee3
Payal Kamble <payalkamble@gmail.com>
2023-01-24 20:32:48 +0000

    Added rest controller and spring-web dependency

---
+++ src/main/java/com/example/springcoder/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.springcoder;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+    @GetMapping("/check")
+	public String check() {
+		return "spring is running is coder env!";
+	}
+
+}
